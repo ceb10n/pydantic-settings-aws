07f87d8bcc36b146849e81417a490aad212b2547
Rafael Marques <rmarques@brasilseg.com.br>
2024-07-27 15:37:06 -0300

    Add AWSBaseSettings

--- pydantic_settings_aws/utils.py
+++ pydantic_settings_aws/utils.py
@@ -1,10 +1,16 @@
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
+
+
+def get_annotated_service_metadata(
+    metadata: List[Any],
+) -> Optional[Dict[str, Any]]:
+    service_metadata = list(filter(_get_service_metadata, metadata))
+
+    return service_metadata[0] if service_metadata else None
 
 
 def get_ssm_name_from_annotated_field(metadata: List[Any]) -> Optional[str]:
-    ssm_metadata = list(
-        filter(_get_ssm_info_from_metadata, metadata)
-    )
+    ssm_metadata = list(filter(_get_ssm_info_from_metadata, metadata))
 
     if ssm_metadata:
         return ssm_metadata[0]
@@ -16,7 +22,14 @@ def _get_ssm_info_from_metadata(metadata: Any) -> Optional[Any]:
     if isinstance(metadata, str):
         return metadata
 
-    if isinstance(metadata, dict) and "ssm" in metadata.keys():
+    if isinstance(metadata, dict) and "ssm" in metadata:
+        return metadata
+
+    return None
+
+
+def _get_service_metadata(metadata: Any) -> Optional[Dict[str, Any]]:
+    if isinstance(metadata, dict) and "service" in metadata:
         return metadata
 
     return None
--- tests/settings_test.py
+++ tests/settings_test.py
@@ -1,9 +1,16 @@
+import os
+
 from .settings_mocks import (
+    AWSWithNonDictMetadata,
+    AWSWithParameterAndSecretsWithDefaultBoto3Client,
+    AWSWithParameterSecretsAndEnvironmentWithDefaultBoto3Client,
+    AWSWithUnknownService,
     MySecretsWithClientConfig,
     ParameterSettings,
     ParameterWithOptionalValueSettings,
     ParameterWithTwoSSMClientSettings,
     SecretsWithNestedContent,
+    dict_secrets_with_username_and_password,
 )
 
 
@@ -47,3 +54,41 @@ def test_ssm_with_none_in_optional_values():
 
     assert my_config is not None
     assert my_config.my_ssm is None
+
+
+def test_aws_with_secrets_and_parameters():
+    my_config = AWSWithParameterAndSecretsWithDefaultBoto3Client()
+
+    assert my_config is not None
+    assert (
+        my_config.username
+        == dict_secrets_with_username_and_password["username"]
+    )
+    assert (
+        my_config.password
+        == dict_secrets_with_username_and_password["password"]
+    )
+    assert my_config.host is not None
+
+
+def test_aws_settings_should_get_value_from_environment_if_not_found_in_ssm_or_secrets():
+    os.environ["server_name"] = "test-server"
+
+    my_config = AWSWithParameterSecretsAndEnvironmentWithDefaultBoto3Client()
+    assert my_config is not None
+    assert my_config.username is not None
+    assert my_config.password is not None
+    assert my_config.host is not None
+    assert my_config.server_name == "test-server"
+
+
+def test_aws_settings_should_ignore_value_if_service_is_unknown():
+    my_config = AWSWithUnknownService()
+    assert my_config is not None
+    assert my_config.my_name is None
+
+
+def test_aws_settings_should_ignore_value_if_metadata_is_not_a_dict():
+    my_config = AWSWithNonDictMetadata()
+    assert my_config is not None
+    assert my_config.my_name is None
